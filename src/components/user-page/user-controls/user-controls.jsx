e8609c329faa603c0ed4b110969f0fa0073fc968
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-26 09:21:38 +0000

    chore: set up non-working email update form

--- src/components/user-page/user-controls/user-controls.jsx
+++ src/components/user-page/user-controls/user-controls.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { deleteRequest } from "../../../common/requests";
 
 const UserControls = (props) => {
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState();
+    const [successMessage, setSuccessMessage] = useState();
+
     const deleteUser = async () => {
         const headers = {authorization: "Bearer " + props.user.loginToken}
         const response = await deleteRequest("http://localhost:5001/users/delete", headers);
@@ -10,6 +15,19 @@ const UserControls = (props) => {
 
     return (
         <>
+        <form id="login-form" className="form-section start-section" onSubmit={(e) => e.preventDefault()}>
+            <h2>Update user settings</h2>
+            <div className="form-item">Email
+            {/* Add onChange function later */}
+                <input id="update-email-input" placeholder="Email address"></input></div> 
+            <button className="green centre" onClick={() => {}}>Update</button>
+        </form>
+
+        { errorMessage &&
+        <div className="error-message">{errorMessage}</div>}
+        { successMessage &&
+        <div className="success-message">{successMessage}</div>}
+
         {/* <p>Update this account</p> */}
         <p>You are logged in as <b>{props.user.username}</b></p> 
         <button className="red" onClick={deleteUser}>Delete This Account</button>
